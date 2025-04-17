0ab54512c683cad1574d2e3efecc5e7abd85c81c
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-17 20:07:00 +0200

    feat: tooltipbuilder, inventorybuilder, itemextensions
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/main/kotlin/tech/thatgravyboat/lib/builder/InventoryBuilder.kt
@@ -0,0 +1,43 @@
+package tech.thatgravyboat.lib.builder
+
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.ItemLike
+import tech.thatgravyboat.lib.extensions.withTooltip
+
+class InventoryBuilder(val maxSize: Int = 54) {
+
+    private val items: MutableMap<Int, ItemStack> = mutableMapOf()
+
+    fun add(slot: Int, item: ItemStack, init: (TooltipBuilder.() -> Unit)? = null) {
+        if (slot >= maxSize || slot < 0) {
+            throw IllegalStateException("Inventory Index out of bounds")
+        }
+        items[slot] = init?.let { item.withTooltip(it) } ?: item
+    }
+
+    fun add(slot: Int, item: ItemLike, init: (TooltipBuilder.() -> Unit)? = null) = add(slot, item.asItem().defaultInstance, init)
+
+    fun add(x: Int, y: Int, item: ItemStack, init: (TooltipBuilder.() -> Unit)? = null) {
+        val slot = x + y * 9
+        if (slot >= maxSize || slot < 0) {
+            throw IllegalStateException("Inventory Index out of bounds")
+        }
+        items[slot] = init?.let { item.withTooltip(it) } ?: item
+    }
+
+    fun add(x: Int, y: Int, item: ItemLike, init: (TooltipBuilder.() -> Unit)? = null) = add(x, y, item.asItem().defaultInstance, init)
+
+    fun fill(item: ItemStack, init: (TooltipBuilder.() -> Unit)? = null) {
+        val item = init?.let { item.withTooltip(it) } ?: item
+        for (i in 0 until maxSize) {
+            if (!items.containsKey(i)) {
+                items[i] = item
+            }
+        }
+    }
+
+    fun fill(item: ItemLike, init: (TooltipBuilder.() -> Unit)? = null) = fill(item.asItem().defaultInstance, init)
+
+    fun build(): List<ItemStack> = items.entries.sortedBy { it.key }.map { it.value }.toList()
+
+}
---
+++ src/main/kotlin/tech/thatgravyboat/lib/extensions/ItemExtensions.kt
@@ -0,0 +1,58 @@
+package tech.thatgravyboat.lib.extensions
+
+import com.mojang.authlib.GameProfile
+import com.mojang.authlib.properties.Property
+import it.unimi.dsi.fastutil.objects.ObjectSortedSets
+import net.minecraft.core.component.DataComponents
+import net.minecraft.network.chat.Component
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import net.minecraft.world.item.component.ItemLore
+import net.minecraft.world.item.component.ResolvableProfile
+import net.minecraft.world.item.component.TooltipDisplay
+import tech.thatgravyboat.lib.builder.TooltipBuilder
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.italic
+import java.util.UUID
+
+fun ItemStack.withoutTooltip() = withTooltip()
+
+fun ItemStack.withTooltip(init: TooltipBuilder.() -> Unit = {}): ItemStack {
+    val builder = TooltipBuilder().apply(init).lines().filterIsInstance<Component>()
+    when {
+        builder.isEmpty() -> this.set(DataComponents.TOOLTIP_DISPLAY, TooltipDisplay(true, ObjectSortedSets.emptySet()))
+        builder.size == 1 -> this.set(
+            DataComponents.CUSTOM_NAME,
+            builder.first().copy().apply {
+                this.italic = false
+            },
+        )
+
+        else -> {
+            this.set(
+                DataComponents.CUSTOM_NAME,
+                builder.first().copy().apply {
+                    this.italic = false
+                },
+            )
+            val lore = builder.drop(1)
+            this.set(DataComponents.LORE, ItemLore(lore, lore))
+        }
+    }
+    return this
+}
+
+object ItemUtils {
+
+    fun createSkull(textureBase64: String): ItemStack {
+        val profile = GameProfile(UUID.randomUUID(), "a")
+        profile.properties.put("textures", Property("textures", textureBase64))
+        return createSkull(profile)
+    }
+
+    fun createSkull(profile: GameProfile): ItemStack {
+        val stack = ItemStack(Items.PLAYER_HEAD)
+        stack.set(DataComponents.PROFILE, ResolvableProfile(profile))
+        return stack
+    }
+}
+
