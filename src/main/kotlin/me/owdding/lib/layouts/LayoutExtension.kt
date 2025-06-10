5fb9c7923fd1f463f19e4c85712626e046d29a90
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-06-10 14:28:55 +0200

    feat: click to expand
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/main/kotlin/me/owdding/lib/layouts/LayoutExtension.kt
@@ -0,0 +1,17 @@
+package me.owdding.lib.layouts
+
+import earth.terrarium.olympus.client.components.compound.LayoutWidget
+import net.minecraft.client.gui.components.AbstractWidget
+import net.minecraft.client.gui.layouts.Layout
+import net.minecraft.client.gui.layouts.LayoutElement
+
+fun LayoutElement.asWidget(): AbstractWidget = when (this) {
+    is AbstractWidget -> this
+    is Layout -> LayoutWidget(this).withStretchToContentSize()
+    else -> throw IllegalArgumentException("Cant convert $this into a widget")
+}
+
+fun Layout.setPos(x: Int, y: Int): Layout {
+    this.setPosition(x, y)
+    return this
+}
