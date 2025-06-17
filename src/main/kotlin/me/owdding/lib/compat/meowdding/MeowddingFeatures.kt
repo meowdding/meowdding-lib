6a647cab0a57ad05653d1261344f3e36f9b24d13
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-06-17 13:47:48 +0200

    fix: hidden config options
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/lib/compat/meowdding/MeowddingFeatures.kt
+++ src/main/kotlin/me/owdding/lib/compat/meowdding/MeowddingFeatures.kt
@@ -3,6 +3,7 @@ package me.owdding.lib.compat.meowdding
 import com.teamresourceful.resourcefulconfig.api.types.ResourcefulConfig
 import com.teamresourceful.resourcefulconfig.api.types.ResourcefulConfigElement
 import com.teamresourceful.resourcefulconfig.api.types.elements.ResourcefulConfigEntryElement
+import com.teamresourceful.resourcefulconfig.api.types.elements.ResourcefulConfigObjectEntryElement
 import com.teamresourceful.resourcefulconfig.common.config.Configurations
 import me.owdding.ktmodules.Module
 
@@ -19,15 +20,17 @@ object MeowddingFeatures {
         configurators.entries.associate { (key, value) -> key to value.getConfig() }
     }
 
-    // todo: config obj dont work, obj keys are in there though (bad i think?)
-
-    private fun ResourcefulConfig.getConfig(): List<String> = buildList {
-        elements().forEach { it.getName()?.let { add(it) } }
-        categories().forEach { addAll(it.value.getConfig()) }
+    private fun ResourcefulConfig.getConfig(): Set<String> = buildSet {
+        elements().forEach { addAll(it.getName()) }
+        categories().filterNot { it.value.info().isHidden }.forEach { addAll(it.value.getConfig()) }
     }
 
-    private fun ResourcefulConfigElement.getName(): String? = when (this) {
-        is ResourcefulConfigEntryElement -> entry().options().title().toLocalizedString()
-        else -> null
+    private fun ResourcefulConfigElement.getName(): List<String> = when (this) {
+        is ResourcefulConfigObjectEntryElement -> {
+            entry().elements().filterNot { it.isHidden }.flatMap { it.getName() }.takeUnless { isHidden } ?: emptyList()
+        }
+
+        is ResourcefulConfigEntryElement -> listOf(entry().options().title().toLocalizedString()).takeUnless { isHidden } ?: emptyList()
+        else -> emptyList()
     }
 }
