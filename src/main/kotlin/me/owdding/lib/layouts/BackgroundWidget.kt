771b782f4f1dd47e0f474d9eb22963e020a01ad0
Mona <59416038+meowora@users.noreply.github.com>
2025-06-29 17:55:41 +0200

    add more background widget

--- src/main/kotlin/me/owdding/lib/layouts/BackgroundWidget.kt
+++ src/main/kotlin/me/owdding/lib/layouts/BackgroundWidget.kt
@@ -1,15 +1,42 @@
 package me.owdding.lib.layouts
 
 import earth.terrarium.olympus.client.components.base.BaseParentWidget
+import me.owdding.lib.displays.Display
+import me.owdding.lib.displays.Displays
+import me.owdding.lib.displays.asLayer
+import me.owdding.lib.displays.asWidget
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.layouts.LayoutElement
-import net.minecraft.client.renderer.RenderType
 import net.minecraft.resources.ResourceLocation
 
-class BackgroundWidget(val background: ResourceLocation, widget: LayoutElement, val padding: Int = 0) : BaseParentWidget(), ContainerBypass {
+class BackgroundWidget(backgroundWidget: LayoutElement, widget: LayoutElement, val padding: Int = 0) : BaseParentWidget(), ContainerBypass {
     val body = widget.asWidget()
+    val background = backgroundWidget.asWidget()
+
+    constructor(background: Display, widget: LayoutElement, padding: Int = 0) : this(background.asWidget(), widget, padding)
+    constructor(resourceLocation: ResourceLocation, widget: LayoutElement, padding: Int = 0) : this(
+        Displays.sprite(
+            resourceLocation,
+            widget.width + padding * 2,
+            widget.height + padding * 2,
+        ),
+        widget, padding,
+    )
+
+    constructor(vararg resourceLocation: ResourceLocation, widget: LayoutElement, padding: Int = 0) : this(
+        resourceLocation.map {
+            Displays.sprite(
+                it,
+                widget.width + padding * 2,
+                widget.height + padding * 2,
+            )
+        }.asLayer(),
+        widget,
+        padding,
+    )
 
     init {
+        this.renderables.add(backgroundWidget.asWidget())
         this.addRenderableWidget(this.body)
     }
 
@@ -17,8 +44,7 @@ class BackgroundWidget(val background: ResourceLocation, widget: LayoutElement, 
     override fun getHeight() = body.height + padding * 2
 
     override fun renderWidget(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTicks: Float) {
-        graphics.blitSprite(RenderType::guiTextured, background, this.x, this.y, body.width + padding * 2, body.height + padding * 2)
-
+        background.setPosition(this.x, this.y)
         body.setPosition(this.x + padding, this.y + padding)
 
         super.renderWidget(graphics, mouseX, mouseY, partialTicks)
