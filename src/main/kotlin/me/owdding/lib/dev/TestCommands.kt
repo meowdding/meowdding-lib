1fe17bfac250afc9547e3aa880f0420f2d860110
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-08-01 12:06:34 +0200

    fix: testcommand
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/lib/dev/TestCommands.kt
+++ src/main/kotlin/me/owdding/lib/dev/TestCommands.kt
@@ -1,17 +1,18 @@
 package me.owdding.lib.dev
 
 import me.owdding.ktmodules.Module
-import me.owdding.lib.waypoints.Waypoint
+import me.owdding.lib.waypoints.MeowddingWaypoint
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterCommandsEvent
 import tech.thatgravyboat.skyblockapi.api.events.render.RenderWorldEvent
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.helpers.McPlayer
+import java.util.*
 
 @Module
 object TestCommands {
 
-    var waypoint: Waypoint? = null
+    var waypoint: MeowddingWaypoint? = null
 
     @Subscription
     fun onCommand(event: RegisterCommandsEvent) {
@@ -23,7 +24,7 @@ object TestCommands {
             }
 
             thenCallback("waypoint") {
-                waypoint = Waypoint(-1, McPlayer.position!!) {
+                waypoint = MeowddingWaypoint(UUID.randomUUID(), McPlayer.position!!) {
                     withName("Test Waypoint")
                     withColor(0xFF00FF00.toInt())
                     withAllRenderTypes()
