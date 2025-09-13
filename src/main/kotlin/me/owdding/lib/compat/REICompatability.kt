648fc8d83b9eba5278f8492dc72c4fab45dba2d7
Siv <80259183+sivthepolarfox@users.noreply.github.com>
2025-09-13 19:09:47 +0200

    feat: Improve REICompat (#22)
    Co-authored-by: ThatGravyBoat <thatgravyboat@gmail.com>
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/lib/compat/REICompatability.kt
+++ src/main/kotlin/me/owdding/lib/compat/REICompatability.kt
@@ -1,12 +1,23 @@
 package me.owdding.lib.compat
 
+import me.owdding.lib.utils.KnownMods
 import me.shedaniel.math.Rectangle
+import me.shedaniel.math.impl.PointHelper
+import me.shedaniel.rei.api.client.REIRuntime
+import me.shedaniel.rei.api.client.gui.screen.DisplayScreen
+import me.shedaniel.rei.api.client.gui.widgets.Slot
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin
 import me.shedaniel.rei.api.client.registry.screen.ExclusionZones
+import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry
+import me.shedaniel.rei.api.common.entry.EntryStack
+import net.minecraft.client.gui.components.events.ContainerEventHandler
+import net.minecraft.client.gui.components.events.GuiEventListener
 import net.minecraft.client.gui.layouts.LayoutElement
 import net.minecraft.client.gui.screens.Screen
+import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI
 import tech.thatgravyboat.skyblockapi.api.events.base.CancellableSkyBlockEvent
+import tech.thatgravyboat.skyblockapi.helpers.McClient
 
 class REIRenderOverlayEvent(val screen: Screen, private val registrar: (Int, Int, Int, Int) -> Unit) : CancellableSkyBlockEvent() {
 
@@ -29,4 +40,31 @@ object REICompatability : REIClientPlugin {
             if (hide) listOf(Rectangle(0, 0, screen.width, screen.height)) else areas
         }
     }
+}
+
+object REIRuntimeCompatability {
+    fun getReiHoveredItemStack(): ItemStack? {
+        if (!KnownMods.REI.installed) return null
+        return runCatching { getItemList() ?: getRecipe() ?: getRecipeFallback() }.getOrNull()
+    }
+
+    private fun getItemList(): ItemStack? {
+        fun getStack(listener: GuiEventListener): ItemStack? = when (listener) {
+            is Slot -> listener.currentEntry.cheatsAs().value
+            !is ContainerEventHandler -> null
+            else -> listener.getChildAt(PointHelper.getMouseFloatingX(), PointHelper.getMouseFloatingY()).orElse(null)?.let(::getStack)
+        }
+
+        val listener = REIRuntime.getInstance().overlay.orElse(null) ?: return null
+        return getStack(listener)
+    }
+
+    private fun getRecipe(): ItemStack? = ScreenRegistry.getInstance().getFocusedStack(McClient.self.screen, PointHelper.ofMouse())?.toStack()
+
+    private fun getRecipeFallback(): ItemStack? {
+        val screen = McClient.self.screen as? DisplayScreen ?: return null
+        return screen.resultsToNotice.firstOrNull()?.toStack()
+    }
+
+    private fun EntryStack<*>.toStack(): ItemStack? = this.value as? ItemStack ?: this.cheatsAs().value
 }
