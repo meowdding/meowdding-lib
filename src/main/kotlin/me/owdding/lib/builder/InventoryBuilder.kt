6342752b61e1eae89e70974b892185b87c27d7ed
Empa <itsempa@users.noreply.github.com>
2025-10-11 21:46:30 +0200

    fix: sophie changes

--- src/main/kotlin/me/owdding/lib/builder/InventoryBuilder.kt
+++ src/main/kotlin/me/owdding/lib/builder/InventoryBuilder.kt
@@ -10,23 +10,21 @@ class InventoryBuilder(val maxSize: Int = 54) {
     private val items: MutableMap<Int, ItemStack> = mutableMapOf()
 
     fun add(slot: Int, item: ItemStack, init: (TooltipBuilder.() -> Unit)? = null) {
-        check(slot !in 0..<maxSize) { "Inventory Index out of bounds" }
+        check(slot !in 0 until maxSize) { "Inventory Index out of bounds" }
         items[slot] = init?.let { item.withTooltip(it) } ?: item
     }
 
     fun add(slot: Int, item: ItemLike, init: (TooltipBuilder.() -> Unit)? = null) = add(slot, item.asItem().defaultInstance, init)
 
     fun add(x: Int, y: Int, item: ItemStack, init: (TooltipBuilder.() -> Unit)? = null) {
-        val slot = x + y * 9
-        check(slot !in 0..<maxSize) { "Inventory Index out of bounds" }
-        items[slot] = init?.let { item.withTooltip(it) } ?: item
+        add(x + y * 9, item, init)
     }
 
     fun add(x: Int, y: Int, item: ItemLike, init: (TooltipBuilder.() -> Unit)? = null) = add(x, y, item.asItem().defaultInstance, init)
 
     fun fill(item: ItemStack, init: (TooltipBuilder.() -> Unit)? = null) {
         val item = init?.let { item.withTooltip(it) } ?: item
-        for (i in 0 until maxSize) {
+        for (i in 0..<maxSize) {
             if (!items.containsKey(i)) {
                 items[i] = item
             }
