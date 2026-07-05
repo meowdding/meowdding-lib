8fc58b9d6a0b5ee56597b2a86b4e465beed701ae
Mona <git@mona.cat>
2026-07-05 02:25:31 +0200

    feat: add pack info

---
+++ src/main/kotlin/me/owdding/lib/utils/PackInfo.kt
@@ -0,0 +1,63 @@
+package me.owdding.lib
+
+import kotlinx.coroutines.runBlocking
+import me.owdding.ktcodecs.FieldName
+import me.owdding.ktcodecs.GenerateCodec
+import me.owdding.ktmodules.Module
+import me.owdding.lib.generated.MeowddingLibCodecs
+import me.owdding.lib.utils.MeowddingLogger
+import me.owdding.lib.utils.MeowddingLogger.Companion.featureLogger
+import net.minecraft.network.protocol.common.ClientboundResourcePackPushPacket
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.level.PacketReceivedEvent
+import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
+import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.http.Http
+import tech.thatgravyboat.skyblockapi.utils.json.Json.toJson
+import java.util.concurrent.CompletableFuture
+
+const val API_URL = "https://skyblock-pack.meowdd.ing"
+
+@Module
+object SkyblockPackInfo : MeowddingLogger by MeowddingLib.featureLogger() {
+    val regex = Regex("(?i)^https://resourcepacks2\\.hypixel\\.net/SkyBlockResourcePack/(?<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/(?<num>\\d{2}).zip$")
+    private val codec = MeowddingLibCodecs.getCodec<PackInfo>()
+
+    @Subscription
+    private fun onPacketReceived(event: PacketReceivedEvent) {
+        if (!LocationAPI.onHypixel) return
+        val packet = event.packet
+        if (packet !is ClientboundResourcePackPushPacket) return
+
+        val match = regex.matchEntire(packet.url) ?: return
+        val uuid = match.groups["uuid"]?.value ?: return
+        val num = match.groups["num"]?.value?.toIntOrNull() ?: return
+
+        val packInfo = PackInfo(
+            uuid, num, McClient.version,
+            LocationAPI.onAlpha,
+        )
+
+        val json = packInfo.toJson(codec) ?: return
+
+        debug("Sending $json to server.")
+        CompletableFuture.runAsync {
+            runBlocking {
+                Http.post(
+                    url = API_URL,
+                    body = json,
+                    handler = { },
+                )
+            }
+        }
+    }
+}
+
+
+@GenerateCodec
+data class PackInfo(
+    @FieldName("uuid") val packId: String,
+    @FieldName("num") val packNumber: Int,
+    @FieldName("version") val gameVersion: String,
+    val alpha: Boolean,
+)
