c0b1aaaa3b1bcb5ddd0936eda62b6859c94a70f1
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-08-17 09:47:22 +0200

    fix: incorrectly doing overlay non chat interaction x2
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/lib/overlays/Overlays.kt
+++ src/main/kotlin/me/owdding/lib/overlays/Overlays.kt
@@ -58,7 +58,7 @@ object Overlays {
             val rect = it.editBounds * it.position.scale
 
             if (isOverlayScreen(screen, mouseX.toInt(), mouseY.toInt()) && rect.contains(mouseX.toInt(), mouseY.toInt())) {
-                if (it.isEditingOverlay() || !it.ignoreChatScreenEdits) return@forEach
+                if (it.isEditingOverlay() || it.ignoreChatScreenEdits) return@forEach
                 graphics.drawFilledBox(rect.x, rect.y, rect.width, rect.height, 0x50000000)
                 graphics.drawOutline(rect.x - 1, rect.y - 1, rect.width + 2, rect.height + 2, 0xFFFFFFFF.toInt())
                 if (it.properties.isNotEmpty()) {
